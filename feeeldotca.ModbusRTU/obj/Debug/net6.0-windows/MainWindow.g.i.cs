2b79e43473985e89b492dced9acef39300b2315b
Kun Zhang <feel@shaw.ca>
2023-07-10 03:27:25 -0600

    ADDED ECLLIPSE

--- feeeldotca.ModbusRTU/obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ feeeldotca.ModbusRTU/obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -53,7 +53,7 @@ namespace feeeldotca.ModbusRTU {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/feeeldotca.ModbusRTU;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/feeeldotca.ModbusRTU;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
