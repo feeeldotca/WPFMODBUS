e65fc0ad3d911af1eb9e1427c34ca99498403eb4
Kun Zhang <feel@shaw.ca>
2023-07-23 00:00:06 -0600

    update

---
+++ feeeldotca.ModbusRTU/ViewModels/ItemsShow.cs
@@ -0,0 +1,22 @@
+﻿using feeeldotca.ModbusRTU.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace feeeldotca.ModbusRTU.ViewModels
+{
+    public class ItemsShow
+    {
+        public List<ItemsModel> ItemList { get; set; } = new List<ItemsModel>();
+        public ItemsShow()
+        {
+            ItemList.Add(new ItemsModel() { SlaveId = 1 }); ;
+            ItemList.Add(new ItemsModel() { SlaveId = 2 });
+            ItemList.Add(new ItemsModel() { SlaveId = 3 });
+            ItemList.Add(new ItemsModel() { SlaveId = 4 });
+        }
+
+    }
+}
--- feeeldotca.ModbusRTU/Views/MainWindow.xaml.cs
+++ feeeldotca.ModbusRTU/Views/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using feeeldotca.ModbusRTU.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,7 @@ namespace feeeldotca.ModbusRTU
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = new ItemsShow();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
