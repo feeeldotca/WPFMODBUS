e36bf92a9ad5275caefd7fa079ca3447e5955b68
Kun Zhang <feel@shaw.ca>
2023-08-04 23:59:55 -0600

    add modules

--- feeeldotca.ModbusRTU/Components/Meter.xaml.cs
+++ feeeldotca.ModbusRTU/Components/Meter.xaml.cs
@@ -20,9 +20,55 @@ namespace feeeldotca.ModbusRTU.Components
     /// </summary>
     public partial class Meter : UserControl
     {
+        public static readonly DependencyProperty ValueProperty = DependencyProperty.Register("Value", typeof(double), typeof(Meter), new PropertyMetadata(0.0));
+
+        public double Value
+        {
+            get { return (double)GetValue(ValueProperty); }
+            set { SetValue(ValueProperty, value); }
+        }
         public Meter()
         {
             InitializeComponent();
+            this.SizeChanged += Meter_SizeChanged;
         }
+
+        private void Meter_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        double radius = 0.0, step = 0.0;
+        private void RefreshScale()
+        {
+            this.root.Width = Math.Min(this.RenderSize.Width, this.RenderSize.Height);
+            this.root.Height = this.root.Width;
+
+            radius = this.root.Height / 2;
+            if (double.IsNaN(radius) || radius <= 0) return;
+
+            scale_canvas.Children.Clear();
+
+            double minValue = -40, maxValue = 80;
+            step = 270.0 / (maxValue - minValue + 24);
+
+            for(int i =0; i< maxValue-minValue +24; i++)
+            {
+                if (i % 12 != 0) continue;
+                double angle = step * i - 45;
+                Line lineScale = new Line();
+                lineScale.Stroke = Brushes.White;
+                lineScale.StrokeThickness = 1;
+                lineScale.Opacity = 0.3;
+
+                lineScale.X1 = radius - (radius - 15) * Math.Cos(angle * Math.PI / 180);
+                lineScale.Y1 = radius - (radius - 15) * Math.Sin(angle * Math.PI / 180);
+                lineScale.X2 = radius - (radius - 15) * Math.Cos(angle * Math.PI / 180);
+                lineScale.Y2= radius - (radius - 15) * Math.Sin(angle * Math.PI / 180);
+
+            }
+
+        }
+
     }
 }
